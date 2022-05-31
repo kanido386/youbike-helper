b421a5fee9f4bbf9089e379622b954c0d8754d1f
kanido386 <kanido386@gmail.com>
2022-05-31 11:59:01 +0800

    feat: can fetch online api json data with node-fetch

--- service/crawler.js
+++ service/crawler.js
@@ -1,8 +1,12 @@
 const cron = require('node-cron');
+const fetch = require('node-fetch');
 
 const initCrawler1 = () => {
-  const crawler1Job = cron.schedule('* * * * *', () => {
+  const crawler1Job = cron.schedule('* * * * *', async () => {
     console.log('crawler1');
+    const response = await fetch('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json');
+    const data = await response.json();
+    console.log(data);
   })
   crawler1Job.start();
 };
